a2e3f570daece3e85173dfc5327e89a298476edd
12DPDSlujevs <106648917+12DPDSlujevs@users.noreply.github.com>
2022-09-27 13:57:06 +0300

    Add files via upload

---
+++ kvadrats.py
@@ -0,0 +1,7 @@
+x = float(input('ievadiet x ass '))
+y = float(input('ievadiet y ass '))
+
+if (x >= -1 and x <= 3) and (y >= -2 and y <= 1):
+    print('ir kvadratā') 
+else:
+    print('nav kvadratā')
